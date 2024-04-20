857185cefdcdc3a04c09dddcc99312ef388e78c3
Shaznan <74103414+shaznan@users.noreply.github.com>
2024-04-20 11:33:02 +0530

    Add description and title for about us and hero section

--- src/components/About-us6/about-us6.jsx
+++ src/components/About-us6/about-us6.jsx
@@ -1,55 +1,55 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 const AboutUs6 = () => {
-    return (
-      <section id="about" className="about-ar section-padding">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-6 valign">
-              <div className="img">
-                <div
-                  className="bg-img bg-fixed hero-bg"
-                  style={{ backgroundImage: "url(/img/arch/hero.jpg)" }}
-                ></div>
-                <div className="exp valign text-center">
-                  <div className="full-width">
-                    <h2
-                      className="bg-img bg-fixed"
-                      style={{ backgroundImage: "url(/img/arch/hero.jpg)" }}
-                    >
-                      25
-                    </h2>
-                    <p>Years Of Experience</p>
-                  </div>
+  return (
+    <section id="about" className="about-ar section-padding">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 valign">
+            <div className="img">
+              <div
+                className="bg-img bg-fixed hero-bg"
+                style={{ backgroundImage: "url(/img/arch/hero.jpg)" }}
+              ></div>
+              <div className="exp valign text-center">
+                <div className="full-width">
+                  <h2
+                    className="bg-img bg-fixed"
+                    style={{ backgroundImage: "url(/img/arch/hero.jpg)" }}
+                  >
+                    25
+                  </h2>
+                  <p>Years Of Experience</p>
                 </div>
               </div>
             </div>
+          </div>
 
-            <div className="col-lg-6 valign">
-              <div className="content">
-                <h6 className="sub-title main-color ls10 text-u">About Us</h6>
-                <h3>Best Designers Architectures for You.</h3>
-                <p>
-                  Architecture bibendum pharetra eleifend. Suspendisse vel
-                  volutpat purus, sit amet bibendum nisl. Cras mollis turpis a
-                  ipsum ultes, nec condimentum ipsum consequat. Mauris vitae
-                  consequat nibh, vitae interdum sit amet bibendum nisl.
-                </p>
+          <div className="col-lg-6 valign">
+            <div className="content">
+              <h6 className="sub-title main-color ls10 text-u">About Us</h6>
+              <h3>Global Elegance, Local Luxury: Our Founding Vision</h3>
+              <p>
+                Founded with the vision of infusing local markets with global
+                luxury, Mega Swiss Holdings has established itself as a premier
+                purveyor of fine fragrances. Our partnership with SPPC, one of
+                France’s most revered perfume houses, enables us to offer an
+                exclusive range of scents that capture the essence of Parisian
+                elegance.
+              </p>
 
-                <Link
-                  href={`/about/about-dark`}
-                >
-                  <a className="butn bord mt-30">
-                    <span>About Us</span>
-                  </a>
-                </Link>
-              </div>
+              <Link href={`/about/about-dark`}>
+                <a className="butn bord mt-30">
+                  <span>About Us</span>
+                </a>
+              </Link>
             </div>
           </div>
         </div>
-      </section>
-    );
-}
+      </div>
+    </section>
+  );
+};
 
-export default AboutUs6
+export default AboutUs6;
