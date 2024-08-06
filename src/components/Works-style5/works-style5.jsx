35d7ecd5aba73d8e4e58d17f9c5f9074822a2dd3
Shaznan <74103414+shaznan@users.noreply.github.com>
2024-08-06 21:41:01 +0530

    remove links

--- src/components/Works-style5/works-style5.jsx
+++ src/components/Works-style5/works-style5.jsx
@@ -10,7 +10,10 @@ const WorksStyle5 = () => {
     };
   }, []);
   return (
-    <section id="product_offerings" className="portfolio po-arch section-padding pb-70" >
+    <section
+      id="product_offerings"
+      className="portfolio po-arch section-padding pb-70"
+    >
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-lg-8 col-md-10">
@@ -54,7 +57,7 @@ const WorksStyle5 = () => {
               <div className="cont">
                 <h5>IvanHoe</h5>
                 <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">EAU DE TOILETTE</Link>
+                  <Link href="">EAU DE TOILETTE</Link>
                 </span>
               </div>
             </div>
@@ -64,7 +67,7 @@ const WorksStyle5 = () => {
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
+                <Link href={``}>
                   <a className="imago wow">
                     <img
                       src="https://firebasestorage.googleapis.com/v0/b/megaswissholdings-4c26c.appspot.com/o/Product%20portfolio%2FPerfume%2FIncidence.png?alt=media&token=4ff459dc-84b5-4cc9-9e5d-a9b0b7937138"
@@ -77,7 +80,7 @@ const WorksStyle5 = () => {
               <div className="cont">
                 <h5>Incidence</h5>
                 <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">EAU DE TOILETTE</Link>
+                  <Link href="">EAU DE TOILETTE</Link>
                 </span>
               </div>
             </div>
@@ -87,7 +90,7 @@ const WorksStyle5 = () => {
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
+                <Link href={``}>
                   <a className="imago wow">
                     <img
                       src="https://firebasestorage.googleapis.com/v0/b/megaswissholdings-4c26c.appspot.com/o/Product%20portfolio%2FPerfume%2FChairman.png?alt=media&token=a67001d3-cae7-4842-bf46-7ad348622274"
@@ -100,7 +103,7 @@ const WorksStyle5 = () => {
               <div className="cont">
                 <h5>Chairman</h5>
                 <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">EAU DE TOILETTE</Link>,
+                  <Link href="">EAU DE TOILETTE</Link>,
                 </span>
               </div>
             </div>
@@ -110,7 +113,7 @@ const WorksStyle5 = () => {
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
+                <Link href={``}>
                   <a className="imago wow">
                     <img
                       src="https://firebasestorage.googleapis.com/v0/b/megaswissholdings-4c26c.appspot.com/o/Product%20portfolio%2FPerfume%2FThallium%20Black.png?alt=media&token=65fe3a01-40e4-4cfb-a6e0-1114a184e7c3"
@@ -123,7 +126,7 @@ const WorksStyle5 = () => {
               <div className="cont">
                 <h5>Thallium Black</h5>
                 <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">EAU DE TOILETTE</Link>
+                  <Link href="">EAU DE TOILETTE</Link>
                 </span>
               </div>
             </div>
@@ -133,7 +136,7 @@ const WorksStyle5 = () => {
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
+                <Link href={``}>
                   <a className="imago wow">
                     <img
                       src="https://firebasestorage.googleapis.com/v0/b/megaswissholdings-4c26c.appspot.com/o/Product%20portfolio%2FPerfume%2FRED%20PEARL.png?alt=media&token=c3e6124c-1c2e-4291-8b17-e696a68d0f47"
@@ -146,7 +149,7 @@ const WorksStyle5 = () => {
               <div className="cont">
                 <h5>Red Pearl</h5>
                 <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">EAU DE TOILETTE</Link>
+                  <Link href="">EAU DE TOILETTE</Link>
                 </span>
               </div>
             </div>
@@ -155,7 +158,7 @@ const WorksStyle5 = () => {
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
+                <Link href={``}>
                   <a className="imago wow">
                     <img
                       src="https://firebasestorage.googleapis.com/v0/b/megaswissholdings-4c26c.appspot.com/o/Product%20portfolio%2FPerfume%2FJoli%20reve%20(1).png?alt=media&token=59c1ab25-a85b-4300-b56f-75a6643f50f7"
@@ -168,7 +171,7 @@ const WorksStyle5 = () => {
               <div className="cont">
                 <h5>Joli Reve</h5>
                 <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">EAU DE TOILETTE</Link>,
+                  <Link href="">EAU DE TOILETTE</Link>,
                 </span>
               </div>
             </div>
@@ -177,7 +180,7 @@ const WorksStyle5 = () => {
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
+                <Link href={``}>
                   <a className="imago wow">
                     <img
                       src="https://firebasestorage.googleapis.com/v0/b/megaswissholdings-4c26c.appspot.com/o/Product%20portfolio%2FPerfume%2FSanderling%20shine%20(1).png?alt=media&token=347daa5f-3e70-4a17-88c4-11347fe65449"
@@ -190,7 +193,7 @@ const WorksStyle5 = () => {
               <div className="cont">
                 <h5>VILLAS IN SOCHI</h5>
                 <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">EAU DE TOILETTE</Link>
+                  <Link href="">EAU DE TOILETTE</Link>
                 </span>
               </div>
             </div>
@@ -199,7 +202,7 @@ const WorksStyle5 = () => {
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
+                <Link href={``}>
                   <a className="imago wow">
                     <img
                       src="https://firebasestorage.googleapis.com/v0/b/megaswissholdings-4c26c.appspot.com/o/Product%20portfolio%2FPerfume%2FAkoya%20blue%20(1).png?alt=media&token=5c54f8bc-9ad6-45cb-8d7c-544648ebae94"
@@ -212,7 +215,7 @@ const WorksStyle5 = () => {
               <div className="cont">
                 <h5>Akoya Blue</h5>
                 <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">EAU DE TOILETTE</Link>,
+                  <Link href="">EAU DE TOILETTE</Link>,
                 </span>
               </div>
             </div>
@@ -222,7 +225,7 @@ const WorksStyle5 = () => {
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
+                <Link href={``}>
                   <a className="imago wow">
                     <img
                       src="https://firebasestorage.googleapis.com/v0/b/megaswissholdings-4c26c.appspot.com/o/Product%20portfolio%2FDeodrants%2FAviator.png?alt=media&token=cc5fa494-6472-4045-83e8-370ada4090df"
@@ -235,8 +238,8 @@ const WorksStyle5 = () => {
               <div className="cont">
                 <h5>Aviator</h5>
                 <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">Deodrant spray</Link>,
-                  <Link href="/works/works-dark">For men</Link>,
+                  <Link href="">Deodrant spray</Link>,
+                  <Link href="">For men</Link>,
                 </span>
               </div>
             </div>
@@ -245,7 +248,7 @@ const WorksStyle5 = () => {
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
+                <Link href={``}>
                   <a className="imago wow">
                     <img
                       src="https://firebasestorage.googleapis.com/v0/b/megaswissholdings-4c26c.appspot.com/o/Product%20portfolio%2FDeodrants%2FChancellor.png?alt=media&token=f3b13380-656c-4dc9-a467-a21cc406b099"
@@ -258,8 +261,8 @@ const WorksStyle5 = () => {
               <div className="cont">
                 <h5>Chancellor</h5>
                 <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">Deodrant spray</Link>,
-                  <Link href="/works/works-dark">For men</Link>,
+                  <Link href="">Deodrant spray</Link>,
+                  <Link href="">For men</Link>,
                 </span>
               </div>
             </div>
@@ -268,7 +271,7 @@ const WorksStyle5 = () => {
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
+                <Link href={``}>
                   <a className="imago wow">
                     <img
                       src="https://firebasestorage.googleapis.com/v0/b/megaswissholdings-4c26c.appspot.com/o/Product%20portfolio%2FDeodrants%2FIvanhoe%20(1).png?alt=media&token=5a0e4d3b-ac89-4fd5-a8a6-384736e6db88"
@@ -281,8 +284,8 @@ const WorksStyle5 = () => {
               <div className="cont">
                 <h5>Ivanhoe</h5>
                 <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">Deodrant spray</Link>,
-                  <Link href="/works/works-dark">For men</Link>,
+                  <Link href="">Deodrant spray</Link>,
+                  <Link href="">For men</Link>,
                 </span>
               </div>
             </div>
@@ -291,7 +294,7 @@ const WorksStyle5 = () => {
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
+                <Link href={``}>
                   <a className="imago wow">
                     <img
                       src="https://firebasestorage.googleapis.com/v0/b/megaswissholdings-4c26c.appspot.com/o/Product%20portfolio%2FDeodrants%2FMondaine.png?alt=media&token=6adc6521-26e7-4045-a0a4-ef84a515810a"
@@ -304,8 +307,8 @@ const WorksStyle5 = () => {
               <div className="cont">
                 <h5>Mondaine</h5>
                 <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">Deodrant spray</Link>,
-                  <Link href="/works/works-dark">For women</Link>,
+                  <Link href="">Deodrant spray</Link>,
+                  <Link href="">For women</Link>,
                 </span>
               </div>
             </div>
@@ -314,7 +317,7 @@ const WorksStyle5 = () => {
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
+                <Link href={``}>
                   <a className="imago wow">
                     <img
                       src="https://firebasestorage.googleapis.com/v0/b/megaswissholdings-4c26c.appspot.com/o/Product%20portfolio%2FDeodrants%2FRichman.png?alt=media&token=37861dfb-8d59-4032-83fc-f3f05d853d70"
@@ -327,8 +330,8 @@ const WorksStyle5 = () => {
               <div className="cont">
                 <h5>Rich man</h5>
                 <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">Deodrant spray</Link>,
-                  <Link href="/works/works-dark">For men</Link>,
+                  <Link href="">Deodrant spray</Link>,
+                  <Link href="">For men</Link>,
                 </span>
               </div>
             </div>
@@ -337,7 +340,7 @@ const WorksStyle5 = () => {
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
+                <Link href={``}>
                   <a className="imago wow">
                     <img
                       src="https://firebasestorage.googleapis.com/v0/b/megaswissholdings-4c26c.appspot.com/o/Product%20portfolio%2FDeodrants%2FVerona.png?alt=media&token=14b8645c-abdd-41bb-9c91-fc40db1e31a4"
@@ -350,8 +353,8 @@ const WorksStyle5 = () => {
               <div className="cont">
                 <h5>Verona</h5>
                 <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">Deodrant spray</Link>,
-                  <Link href="/works/works-dark">For women</Link>,
+                  <Link href="">Deodrant spray</Link>,
+                  <Link href="">For women</Link>,
                 </span>
               </div>
             </div>
@@ -360,7 +363,7 @@ const WorksStyle5 = () => {
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
+                <Link href={``}>
                   <a className="imago wow">
                     <img
                       src="https://firebasestorage.googleapis.com/v0/b/megaswissholdings-4c26c.appspot.com/o/Product%20portfolio%2FDeodrants%2FWriter%20gold.png?alt=media&token=d7723a33-a3d5-4037-8111-e758bfc139fa"
@@ -373,8 +376,8 @@ const WorksStyle5 = () => {
               <div className="cont">
                 <h5>Writer Gold</h5>
                 <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">Deodrant spray</Link>,
-                  <Link href="/works/works-dark">For men</Link>,
+                  <Link href="">Deodrant spray</Link>,
+                  <Link href="">For men</Link>,
                 </span>
               </div>
             </div>
